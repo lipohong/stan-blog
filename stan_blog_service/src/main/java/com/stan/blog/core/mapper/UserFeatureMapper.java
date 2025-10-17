3ad9e13e6f2a9ccfc74f55cdab56016cbfd4242f
Stan Li <lipohong@hotmail.com>
2025-10-17 18:33:15 +0800

    refactor(mappers): remove unused mapper files and XML configurations
    Clean up obsolete mapper interfaces and their corresponding XML files that are no longer needed in the codebase. This includes various entity mappers and content-related mapper configurations that have been deprecated or replaced by other implementations.

--- stan_blog_service/src/main/java/com/stan/blog/core/mapper/UserFeatureMapper.java
+++
@@ -1,7 +0,0 @@
-package com.stan.blog.core.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.stan.blog.beans.entity.user.UserFeatureEntity;
-
-public interface UserFeatureMapper extends BaseMapper<UserFeatureEntity> {
-}
--- stan_blog_service/src/main/java/com/stan/blog/file/mapper/FileResourceMapper.java
+++
@@ -1,8 +0,0 @@
-package com.stan.blog.file.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.stan.blog.beans.entity.file.FileResourceEntity;
-
-public interface FileResourceMapper extends BaseMapper<FileResourceEntity> {
-}
-
